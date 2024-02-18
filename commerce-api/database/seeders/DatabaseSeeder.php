d65fb1e4c7c8efa58ef588066cf68b6ff496ff86
Isaac Alfredo <isaacfreitas23@gmail.com>
2024-02-18 20:40:30 -0300

    OrderFactory

--- commerce-api/database/seeders/DatabaseSeeder.php
+++ commerce-api/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ProductSeeder::class);
-        $this->call(UserSeeder::class);
+        //$this->call(ProductSeeder::class);
+        //$this->call(UserSeeder::class);
+        $this->call(OrderSeeder::class);
     }
 }
